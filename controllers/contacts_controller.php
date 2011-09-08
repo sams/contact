a68dbd83fd5d52263aa1b68ddbd639800abebfd4
unknown <sam.sherlock@gmail.com>
2011-09-08 07:11:48 +0100

    will now render ctp found in views/contact/ if found

--- controllers/contacts_controller.php
+++ controllers/contacts_controller.php
@@ -17,47 +17,78 @@ class ContactsController extends ContactAppController {
 	 */
 	function add() {
 		if ($this->RequestHandler->isGet()) {
-			return;
+			return parent::render($this->action, 'default', VIEWS . 'contact' . DS . $this->action . '.ctp');
 		}
-
-		$this->Contact->set($this->data);
-		if (!$this->Contact->validates()) {
-			return $this->Session->setFlash(
-				__d('contacts', "Please fill-in all required fields", true),
-				'message_notice');
+		
+		if(!empty($this->data)) {
+			$this->Contact->set($this->data);
+			if (!$this->Contact->validates()) {
+				return $this->Session->setFlash(
+					__d('contacts', "Please fill-in all required fields", true),
+					'message_notice');
+			}
+	
+			if (!$this->Contact->save($this->data, false)) {
+				return $this->Session->setFlash(
+					__d('contacts', "An error occured while saving", true),
+					'message_error');
+			}
+	
+			$this->Email->reset();
+			if (Configure::read('debug') > 0) {
+				$this->Email->delivery = 'debug';
+			}
+			$this->Email->to = Configure::read('Contact.email');
+			$this->Email->from = $this->data['Contact']['email'];
+			$this->Email->replyTo = $this->data['Contact']['email'];
+			$this->Email->subject = __d('contacts', 'New Contact', true);
+			$this->Email->template = 'add';
+			$this->Email->sendAs = 'text';
+			$this->set('contact', $this->data);
+			$this->Email->send();
+	
+			$this->Session->setFlash(
+				__d('contacts', 'Your message was sent successfully.', true),
+				'message_success');
+	
+			$this->redirect(array('action' => 'thanks'));
+		} else {
+		  $this->render($this->action);
 		}
-
-		if (!$this->Contact->save($this->data, false)) {
-			return $this->Session->setFlash(
-				__d('contacts', "An error occured while saving", true),
-				'message_error');
-		}
-
-		$this->Email->reset();
-		if (Configure::read('debug') > 0) {
-			$this->Email->delivery = 'debug';
-		}
-		$this->Email->to = Configure::read('Contact.email');
-		$this->Email->from = $this->data['Contact']['email'];
-		$this->Email->replyTo = $this->data['Contact']['email'];
-		$this->Email->subject = __d('contacts', 'New Contact', true);
-		$this->Email->template = 'add';
-		$this->Email->sendAs = 'text';
-		$this->set('contact', $this->data);
-		$this->Email->send();
-
-		$this->Session->setFlash(
-			__d('contacts', 'Your message was sent successfully.', true),
-			'message_success');
-
-		$this->redirect(array('action' => 'thanks'));
 	}
 
 	/**
-	 * Create a app/views/plugins/contacts/contacts/thanks.ctp
+	 * Create a app/views/contact/thanks.ctp
 	 * to customize your thanks page
 	 */
 	function thanks() {
+		$this->render($this->action);
+	}
+	
+	/**
+	 * function render
+	 * 
+	 */
+	
+	public function render($action = null, $layout = null, $file = null, $themed = null) {
+	  
+	  
+		if(is_null($action)) $action = $this->action;
+		if(is_null($layout)) $layout = 'default';
+		
+		debug(func_get_args());
+		debug(array($action, $layout, $file, $themed));
+		
+		if (!is_null($themed) && file_exists(VIEWS . 'themed' . DS . $themed . DS . 'contact' . DS . $action . '.ctp')) {
+		  echo "file: 1";
+			if(is_null($file)) $file = VIEWS . 'themed' . DS . $themed . DS . 'contact' . DS . $action . '.ctp';
+			return parent::render($action, $layout, $file);
+		}
+		if (!file_exists(VIEWS . 'contact' . DS . $this->action . '.ctp')) {
+		  echo "file: " . VIEWS . 'contact' . DS . $this->action . '.ctp';
+		  return false;
+		}
+		return parent::render($action, $layout, $file);
 	}
 }
 ?>
